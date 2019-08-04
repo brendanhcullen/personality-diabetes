1ed9de5d4750d6e3024371a1c3f7ac898fa92f2a
Brendan Cullen <bcullen@uoregon.edu>
2019-08-03 18:44:35 -0700

    create single dataset with demographics, 135 raw spi items, and spi_5 and spi_27 scores

--- src/clean_data.R
+++ src/clean_data.R
@@ -6,9 +6,8 @@ library(psych)
 library(here)
 library(janitor)
 
-
 # Load dataset ------------------------------------------------------------
-source(here("src/toy_dataset.R"))
+source(here("src/build_toy_data.R"))
 
 # Clean SPI data ----------------------------------------------------------
 
@@ -21,21 +20,18 @@ spi_135_items <- toydata %>%
 # only retain personality items that corespond to SPI 135
 spi_135_indices <- which(names(toydata) %in% spi_135_items)
 
-toydata_demographics <- toydata %>% select(-starts_with("q_"))
-toydata_spi_135 <- toydata %>% select(spi_135_indices) 
+toydata_demographics <- toydata %>% select(-starts_with("q_")) # demographic vars only
+toydata_spi_135 <- toydata %>% select(spi_135_indices) # spi items only
 
-toy2 <- cbind(toydata_demographics, toydata_spi_135)
+toydata2 <- cbind(toydata_demographics, toydata_spi_135)
+rm(list = c("toydata_demographics", "toydata_spi_135", "spi_135_items", "spi_135_indices"))
 
 # score SPI_5 and SPI_27
-pers_scores <- scoreVeryFast(spi.keys, toy2) %>% 
+pers_scores <- scoreVeryFast(spi.keys, toydata2) %>% # note: psych::scoreItems gives lots of reliability stats
   as.data.frame() %>% 
   clean_names()
-
-pers_scores2 <- scoreItems(spi.keys, toy2) %>% 
-  as.data.frame() %>% 
-  clean_names()
-
 
 # combine
-toy3 <- cbind(toy2, pers_scores)
+toydata_scored <- cbind(toydata2, pers_scores) 
+rm(list = c("toydata2", "pers_scores", "toydata"))
 

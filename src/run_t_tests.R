d19fcf6a7fee6f126339e226ad37f49e911b54cc
Brendan Cullen <bcullen@uoregon.edu>
2019-08-13 15:28:21 -0700

    run multiple t tests and do Holm correction

--- src/run_t_tests.R
+++ src/run_t_tests.R
@@ -18,15 +18,24 @@ data_nested <- data_scored %>%
   nest()
 
 # organize dataframe to run t-tests
-data_ <- expand.grid(
+data_nested <- expand.grid(
   comparison = c("t1.v.healthy", "t2.v.healthy", "t1.v.t2"),
-  trait = df$trait,
+  trait = data_nested$trait,
   stringsAsFactors = FALSE) %>% 
-  left_join(df) %>% 
+  left_join(data_nested) %>% 
   mutate(data = case_when(comparison == "t1.v.t2" ~ map(data, ~filter(.x, !diagnosis == "healthy")),
                           comparison == "t1.v.healthy" ~ map(data, ~filter(.x, !diagnosis == "t2d")),
                           comparison == "t2.v.healthy" ~ map(data, ~filter(.x, !diagnosis == "t1d"))))
 
 
-t.test(agree ~ diagnosis, data = data_scored %>% filter(!diagnosis == "t2d")) %>% 
-  tidy()
+
+# Run multiple t-tests ----------------------------------------------------
+
+# run t-test for each personality trait variable
+t_test_output <- data_nested %>% 
+  mutate(t_test = map(data, ~broom::tidy(t.test(score ~ diagnosis, data = .)))) %>% 
+  select(-data) %>% 
+  unnest() 
+
+adj_p <- t_test_output %>% 
+  mutate(p.value = p.adjust(p.value, method = "holm"))

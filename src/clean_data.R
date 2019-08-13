f1d3722d030ca332f8ae1a6a4b5ae0cdcafecb0c
Brendan Cullen <bcullen@uoregon.edu>
2019-08-12 17:59:41 -0700

    change char vars to factors

--- src/clean_data.R
+++ src/clean_data.R
@@ -6,6 +6,10 @@ source(here("src/helper_functions.R"))
 source(here("src/build_toy_data.R"))
 data <- toydata
 
+# score SPI data
 data_scored <- scored_data <- score_spi(data)
+
+# change char vars to factors 
+data_scored <- fix_var_types(data_scored)
 
 save(data_scored, file = here("output/data_cleaned.Rdata"))

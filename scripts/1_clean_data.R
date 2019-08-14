303f06a346950a01438aaaed86e30d94456bd7cf
Brendan Cullen <bcullen@uoregon.edu>
2019-08-14 10:59:22 -0700

    add notes

--- scripts/1_clean_data.R
+++ scripts/1_clean_data.R
@@ -1,15 +1,33 @@
+# This script imports raw SAPA data and performs basic cleaning operations:
+# 1) score raw SPI items according to 5- and 27-factor solutions
+# 2) convert varaibles to correct type (e.g. factors)
+# 3) create composite demographic variables???
+# 4) impute missing data???
 
 # import helper functions
 source(here("src/helper_functions.R"))
+
+
+# Import data -------------------------------------------------------------
 
 # import toydataset to use for writing analysis code
 source(here("src/build_toy_data.R"))
 data <- toydata
 
-# score SPI data
+# when importing real dataset use this:
+#data <- retrieve_data("doi:10.7910/DVN/TZJGAT", "sapaTempData696items22dec2015thru07feb2017.tab")
+
+
+# Score SPI-135 data ----------------------------------------------------------
+
 data_scored <- scored_data <- score_spi(data)
+
+# Fix variable types ------------------------------------------------------
 
 # change char vars to factors 
 data_scored <- fix_var_types(data_scored)
+
+
+# Save cleaned data -------------------------------------------------------
 
 save(data_scored, file = here("output/data_cleaned.Rdata"))

ee8c4365f7e4c7b66e50e1e1ffc7aca9b5cc5122
Brendan Cullen <bcullen@uoregon.edu>
2019-08-16 13:58:31 -0700

    functions to create and save plots

--- scripts/fig_t_test_output.R
+++ scripts/fig_t_test_output.R
@@ -1,8 +1,8 @@
 # This script plots the output from t-tests comparing diabetes groups on 5- and 27-factor SPI scores.
 
+# load libraries
 library(here)
 library(tidyverse)
-
 
 # Load output from t-tests ------------------------------------------------
 
@@ -18,11 +18,40 @@ t_test_output <- t_test_output %>%
   mutate(trait = factor(trait) %>% 
            fct_reorder(cohens_d))
 
-big5 <- c("open", "consc", "extra", "agree", "neuro")
+spi5 <- c("open", "consc", "extra", "agree", "neuro")
+spi27 <- c(
+  "compassion",
+  "trust",
+  "honesty",
+  "conservatism",
+  "authoritarianism",
+  "easy_goingness",
+  "perfectionism",
+  "order",
+  "industry",
+  "impulsivity",
+  "self_control",
+  "emotional_stability",
+  "anxiety",
+  "irritability",
+  "well_being",
+  "emotional_expressiveness",
+  "sociability",
+  "adaptability",
+  "charisma",
+  "humor",
+  "attention_seeking",
+  "sensation_seeking",
+  "conformity",
+  "introspection",
+  "art_appreciation",
+  "creativity",
+  "intellect"
+)
 
 # plot results for big 5 traits
 t_test_output %>% 
-  filter(trait %in% big5) %>% 
+  filter(trait %in% spi5) %>% 
   ggplot(aes(trait, cohens_d)) +
   geom_errorbar(aes(ymin = d_conf_low,
                     ymax = d_conf_high,
@@ -31,11 +60,42 @@ t_test_output %>%
   coord_flip() + 
   facet_wrap(~comparison) + 
   labs(x = "", y = "Effect size (d)") +
-  #theme_minimal(base_size = 17) + 
   theme(legend.position = "none",
         panel.grid.minor = element_blank())
 
 
+plot_d <- function(spi_traits) {
+  plot <- t_test_output %>% 
+  filter(trait %in% spi_traits) %>% 
+  ggplot(aes(trait, cohens_d)) +
+  geom_errorbar(aes(ymin = d_conf_low,
+                    ymax = d_conf_high,
+                    width = 0.15)) +
+  geom_point(aes(color = trait), size = 4) +
+  coord_flip() + 
+  facet_wrap(~comparison) + 
+  labs(x = "", y = "Effect size (d)") +
+  theme(legend.position = "none",
+        panel.grid.minor = element_blank())
+  
+  return(plot)
+}
+
+spi5_plot <- plot_d(spi5)
+spi27_plot <- plot_d(spi27)
+
 # Save plot ---------------------------------------------------------------
 
-ggsave("effect_sizes.png", width = 15, height = 10, path = here("figs"))
+save_plot <- function(num_traits) {
+ # plot <- paste0("spi", num_traits, "_plot")
+  
+  ggsave(paste0("spi_", num_traits, "_effsizes", ".png"),
+         plot = get(paste0("spi", num_traits, "_plot")),
+         width = 15,
+         height = 10, 
+         path = here("figs"))
+}
+
+save_plot(5)
+save_plot(27)
+

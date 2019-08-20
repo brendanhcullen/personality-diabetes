9bba4582aa43d38db56cb52ece2f29417b16d2e9
Brendan Cullen <bcullen@uoregon.edu>
2019-08-19 23:08:13 -0700

    split into training and holdout

--- scripts/4_machine_learning.R
+++ scripts/4_machine_learning.R
@@ -8,8 +8,24 @@ library(caret)
 
 load(here("output/data_cleaned.Rdata"))
 
-# Wrangle data ------------------------------------------------------------
+set.seed(081919)
 
+# Wrangle data and partition -----------------------------------------------
+
+# data wrangling
 data_ml <- data_scored %>% 
-  select(c(RID, diagnosis, starts_with("q_")))
+  select(c(RID, diagnosis, starts_with("q_"))) %>% 
+  mutate(diagnosis = as.factor(diagnosis))
+
+# partition into training (80%) and testing (20%)
+train_index <- createDataPartition(data_ml$diagnosis,
+                                   times = 1,
+                                   p = .8,
+                                   list = FALSE)
+
+train_data <- data_ml[train_index, ] # training data
+test_data <- data_ml[-train_index, ] # holdout testing data
+
+# Logistic regression -----------------------------------------------------
+
 

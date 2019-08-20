9cbe29dfc2c104362a4a7723b7d27002659af473
Brendan Cullen <bcullen@uoregon.edu>
2019-08-20 14:38:51 -0700

    logistic regression first pass

--- scripts/4_machine_learning.R
+++ scripts/4_machine_learning.R
@@ -18,13 +18,26 @@ data_ml <- data_scored %>%
   mutate(diagnosis = as.factor(diagnosis))
 
 # partition into training (80%) and testing (20%)
-train_index <- createDataPartition(data_ml$diagnosis,
+partition <- createDataPartition(data_ml$diagnosis,
                                    times = 1,
                                    p = .8,
                                    list = FALSE)
 
-train_data <- data_ml[train_index, ] # training data
-test_data <- data_ml[-train_index, ] # holdout testing data
+dev_set <- data_ml[partition, ] # development set
+holdout_set <- data_ml[-partition, ] # holdout set
+
+# partiton initial development data into train and test data
+partition2 <- createDataPartition(dev_set$diagnosis,
+                                   times = 1,
+                                   p = .7,
+                                   list = FALSE)
+
+training <- dev_set[partition2, ]
+testing <- dev_set[-partition2, ]
+
+kfold <- trainControl(method = "repeatedcv",
+                      number = 10, 
+                      repeats = 10)
 
 # Logistic regression -----------------------------------------------------
 

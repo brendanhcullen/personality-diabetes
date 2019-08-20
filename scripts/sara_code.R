c4094605da7f4956bddc5d0051ac23e26504c59d
Sara Weston <weston.sara@gmail.com>
2019-08-20 14:35:32 -0700

    practice code

---
+++ scripts/sara_code.R
@@ -0,0 +1,37 @@
+
+library(here)
+library(tidyverse)
+library(caret)
+library(Hmisc)
+
+# Load data ---------------------------------------------------------------
+
+load(here("output/data_cleaned.Rdata"))
+
+set.seed(081919)
+
+# Wrangle data and partition -----------------------------------------------
+
+# data wrangling
+data_ml <- data_scored %>% 
+  select(c(RID, diagnosis, starts_with("q_"))) %>% 
+  mutate(diagnosis = as.factor(diagnosis))
+
+# partition into training (80%) and testing (20%)
+train_index <- createDataPartition(data_ml$diagnosis,
+                                   times = 1,
+                                   p = .8,
+                                   list = FALSE)
+
+train_data <- data_ml[train_index, ] # training data
+test_data <- data_ml[-train_index, ] # holdout testing data
+
+# Impute missing data -----------------------------------------------------
+
+train_data = train_data %>%
+  dplyr::select(-RID) %>%
+  mutate_all(impute, fun = "random")
+
+# Logistic regression -----------------------------------------------------
+
+

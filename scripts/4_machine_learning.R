6d5e21f2831343414f8ab4654a279c59a71695ea
Brendan Cullen <bcullen@uoregon.edu>
2019-08-25 17:20:34 -0700

    remove second partition of data and minor tweaks to logist regression example

--- scripts/4_machine_learning.R
+++ scripts/4_machine_learning.R
@@ -5,6 +5,7 @@ library(caret)
 library(nnet)
 library(Hmisc)
 library(e1071)
+library(DMwR) # for smote sampling
 
 
 # Load data ---------------------------------------------------------------
@@ -26,39 +27,35 @@ partition <- createDataPartition(data_ml$diagnosis,
                                    p = .8,
                                    list = FALSE)
 
-dev_set <- data_ml[partition, ] # development set
-holdout_set <- data_ml[-partition, ] # holdout set
+train_data <- data_ml[partition, ] # training data (note: this will be iteratively split into train and validation sets during k-fold cross-validation.)
+test_data <- data_ml[-partition, ] # holdout test data. Don't use this until evaluating final model performance.
 
-# partiton initial development data into train and test data
-partition2 <- createDataPartition(dev_set$diagnosis,
-                                   times = 1,
-                                   p = .7,
-                                   list = FALSE)
-
-training <- dev_set[partition2, ]
-testing <- dev_set[-partition2, ]
-
-cv <- trainControl(method = "repeatedcv",
-                      number = 2, 
-                      repeats = 2)
+# specify resampling parameters
+train_control <- trainControl(method = "repeatedcv",
+                   number = 2, # number of folds = 10
+                   repeats = 2, # cross-validation is repeated 10 times
+                   sampling = "up", # use for resolving class imbalances (up, down, smote) 
+                   returnResamp = "final") # only return final 
 
 # Impute missing data -----------------------------------------------------
 
-# randomly impute for now...update this real imputation code later!
-training <- training %>% 
+# manually randomly impute for now. Note: imputation will eventually occur as a "pre-processing" step
+train_data <- train_data %>% 
   mutate_all(Hmisc::impute, fun = "random") %>% 
   filter(row_number() %in% sample(1:nrow(.), size = 5000, replace = FALSE))
   
-
 # Logistic regression -----------------------------------------------------
+pmr_grid = expand.grid(decay = seq(from = 0, to = .5, by = .1))
 
-pmr <- train(diagnosis ~ .,
-      data = training,
+pmr_fit <- train(diagnosis ~ .,
+      data = train_data,
       method = "multinom", 
-      trControl = cv,
-      na.action = "na.exclude")
+      trControl = train_control,
+      tuneGrid = pmr_grid,
+      metric = "Kappa")
 
-pmr
+confusionMatrix(pmr_fit)
 
-summary(pmr)
+pmr_fit$finalModel
+
 

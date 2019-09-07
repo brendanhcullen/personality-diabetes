7fa27c5e896883bc9d1e19a4dd4d395a65a39a2a
Brendan Cullen <bcullen@uoregon.edu>
2019-09-07 14:35:47 -0700

    write individual scripts so that they load in train_data and train_control objects

--- scripts/4.2_train_models.R
+++ scripts/4.2_train_models.R
@@ -12,30 +12,55 @@ train_control = readRDS(here("/output/machine_learning/training/train_control.RD
 
 
 create_script = function(ml_model, tuning_grid, spi_scoring, train_data) {
-  script = "
+  script = "# This script applies the machine learning algorithm 'ml_model_name' using the 'spi_scoring_name' dataset as input features
+
+# load libraries
+library(tidyverse)
 library(caret)
 
+# load in relevant info for model training
+train_master_df = readRDS(here('/output/machine_learning/training/train_master_df.RDS'))
+train_control = readRDS(here('/output/machine_learning/training/train_control.RDS'))
+
+# select training data 
+train_data = train_master_df %>% 
+  filter(ml_model == deparse(substitute(ml_model_name)) & spi_scoring == deparse(substitute(spi_scoring_name))) %>% 
+  select(train_data) %>% 
+  map_df(1)
+
+# select tuning grid  
+tuning_grid = train_master_df %>% 
+  filter(ml_model == deparse(substitute(ml_model_name)) & spi_scoring == deparse(substitute(spi_scoring_name))) %>% 
+  select(tuning_grid) %>% 
+  map_df(1)
+
+# train the model 
 model = train(diagnosis ~ .,
                data = train_data,
-               method = ml_model, 
+               method = deparse(substitute(ml_model_name)), 
                trControl = train_control,
                tuneGrid = tuning_grid,
                metric = 'Kappa')
 
-filename = 'ml_model_spi_scoring_fit.RDS'
-output_dir = here('output/machine_learning/training/')
+# specify where to save model output
+filename = 'ml_model_name_spi_scoring_name_fit.RDS'
+output_dir = here('output/machine_learning/training/model_fits/')
 
+# save model output
 saveRDS(model, file = paste0(output_dir, filename)) 
 "
-  new_script = gsub("train_data", train_data, script, fixed = TRUE) %>% 
-    gsub("ml_model", ml_model, ., fixed = TRUE) %>% 
-    gsub("tuning_grid", tuning_grid, ., fixed = TRUE) %>% 
-    gsub("spi_scoring", spi_scoring, ., fixed = TRUE)
-  
+  # substitute relevant strings
+  new_script = gsub("ml_model_name", ml_model, script) %>% 
+    gsub("spi_scoring_name", spi_scoring, .) 
+    
+  # specify where to save resulting .R script
   filename = paste0(ml_model, "_", spi_scoring, ".R")
-  output_dir = here("output/machine_learning/training/")
+  output_dir = here("output/machine_learning/training/scripts/")
   
+  # create .R script
   write_file(new_script, path = paste0(output_dir, filename))
 }
 
+# create .R scripts for all machine learning models
 pmap(train_master_df, create_script)
+

4bcaaccffabd3e3accefe7d9246b607fa5506cd6
Brendan Cullen <bcullen@uoregon.edu>
2019-09-07 13:36:27 -0700

    add full file path for saving model output in individual scripts

--- scripts/4.2_train_models.R
+++ scripts/4.2_train_models.R
@@ -13,20 +13,24 @@ train_control = readRDS(here("/output/machine_learning/training/train_control.RD
 
 create_script = function(ml_model, tuning_grid, spi_scoring, train_data) {
   script = "
-  library(caret)
-  model = train(diagnosis ~ .,
-                 data = train_data,
-                 method = ml_model, 
-                 trControl = train_control,
-                 tuneGrid = tuning_grid,
-                 metric = 'Kappa')
-                 
-  saveRDS(model, file = 'ml_model_spi_scoring_fit.RDS') 
+library(caret)
+
+model = train(diagnosis ~ .,
+               data = train_data,
+               method = ml_model, 
+               trControl = train_control,
+               tuneGrid = tuning_grid,
+               metric = 'Kappa')
+
+filename = 'ml_model_spi_scoring_fit.RDS'
+output_dir = here('output/machine_learning/training/')
+
+saveRDS(model, file = paste0(output_dir, filename)) 
 "
-  new_script = gsub("train_data", train_data, script)
-  new_script = gsub("ml_model", ml_model, new_script)
-  new_script = gsub("tuning_grid", tuning_grid, new_script)
-  new_script = gsub("spi_scoring", spi_scoring, new_script)
+  new_script = gsub("train_data", train_data, script, fixed = TRUE) %>% 
+    gsub("ml_model", ml_model, ., fixed = TRUE) %>% 
+    gsub("tuning_grid", tuning_grid, ., fixed = TRUE) %>% 
+    gsub("spi_scoring", spi_scoring, ., fixed = TRUE)
   
   filename = paste0(ml_model, "_", spi_scoring, ".R")
   output_dir = here("output/machine_learning/training/")

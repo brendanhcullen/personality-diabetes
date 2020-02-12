9b20c15d803d477ccc161fba4a583474f0b8af62
Brendan Cullen <bcullen@uoregon.edu>
2020-02-12 09:33:05 -0800

    change pattern object to indicate all scripts

--- scripts/4.3_run_training_scripts.R
+++ scripts/4.3_run_training_scripts.R
@@ -6,8 +6,8 @@ scripts_dir = here("output/machine_learning/training/scripts")
 logs_dir = here("output/machine_learning/training/logs")
 
 # use to indicate which set of scripts you want to run. 
-pattern = "spi_5"
-#pattern = "*"
+#pattern = "spi_5"
+pattern = "*"
 
 scripts = list.files(scripts_dir, full.names = TRUE, pattern = pattern)
 script_names = script_names = list.files(scripts_dir, full.names = FALSE, pattern = pattern)

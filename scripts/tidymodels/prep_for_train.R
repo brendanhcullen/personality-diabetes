2d14175b477b13e1632067e95ea9d615259f6f95
Brendan Cullen <bcullen@uoregon.edu>
2020-07-16 22:25:46 -0700

    create script that reads in recipes and model specs and creates a master training data frame that contains all info needed to generate individual training scripts

--- scripts/tidymodels/prep_for_train.R
+++ scripts/tidymodels/prep_for_train.R
@@ -6,49 +6,36 @@ library(here)
 library(tidyverse)
 library(tidymodels)
 
-# Model specs -------------------------------------------------------------
 
-# decision tree
-tree_spec <- 
-  decision_tree() %>%
-  set_engine("rpart") %>%
-  set_mode("classification")
+# Load recipes and model specs --------------------------------------------
 
-# random forest
-rf_spec <- 
-  rand_forest() %>% 
-  set_engine("ranger", 
-             num.threads = cores) %>% 
-  set_mode("classification") %>% 
-  set_args(mtry = tune(),
-           min_n = tune(),
-           trees = 1000)
-
-# k-nearest neighbor
-knn_spec <- 
-  nearest_neighbor() %>%
-  set_engine("kknn") %>%
-  set_mode("classification")
-
-# linear discriminant analysis
-lda_spec <- 
-  discrim_linear() %>% 
-  set_engine("MASS") %>% 
-  set_mode("classification")
+recipes <- readRDS(here("output", "tidymodels", "recipes.RDS"))
+model_specs <- readRDS(here("output", "tidymodels", "model_specs.RDS"))
 
 # Create master df --------------------------------------------------------
 
-spi_scoring <- c(
-  "spi_5",
-  "spi_27",
-  "spi_135"
-)
+# pull abbreviated model names out of model specs
+model_names <- map_chr(names(model_specs), ~str_replace(., "_spec", ""))
 
-rec_names <- map_chr(spi_scoring, ~paste("rec", ., sep = "_"))
+# spi scoring versions
+spi_versions <- c("spi_5", "spi_27", "spi_135")
 
-model_names <- c( 
-  "multinom",
-  "rf"
-  )
+# create master data frame that has all the info required to generate individual training scripts
+train_master_df <- 
+  # create all combinations of models and spi scorings
+  expand_grid(model_name = model_names, spi_scoring = spi_versions) %>% 
+  # add list column with recipes
+  mutate(recipe = rep_len(recipes, length.out = length(spi_scoring))) %>% 
+  # add list column with model specs
+  mutate(model_spec = rep(model_specs, each = length(spi_versions))) %>% 
+  # add list column with workflows that bundle recipes and model specs
+  mutate(workflow = map2(model_spec, recipe, ~workflow() %>% add_model(.x) %>% add_recipe(.y)))
 
-expand_grid(spi_scoring, model_names)
+# add unique names to each workflow object
+names(train_master_df$workflow) <- 
+  map2_chr(train_master_df$model_name, 
+           train_master_df$spi_scoring, 
+           ~paste(.x, .y, "wflow", sep = "_"))
+
+# Save data ---------------------------------------------------------------
+saveRDS(train_master_df, file = here("output", "tidymodels", "train_master_df.RDS"))

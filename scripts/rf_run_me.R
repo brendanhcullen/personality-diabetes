f1339815a1545ead6cae6599e4b2e457e0713491
Brendan Cullen <bcullen@uoregon.edu>
2021-01-01 14:03:50 -0800

    add some more comments

--- scripts/rf_run_me.R
+++ scripts/rf_run_me.R
@@ -1,6 +1,12 @@
+# This script automates the process of running the 3 random forest models. 
+# Model fits are saved to the directory `personality-diabetes/output/tidymodels/model_fits`
+# Be sure to have the most recent version of tidymodels installed (otherwise might
+# run into errors)
+
 library(here)
 
-# prep for training
+# prep for training (this will create training scripts for all models, but only 
+# random forest models are run in the next section)
 source(here("scripts/tidymodels/1_clean.R"))
 source(here("scripts/tidymodels/2_split_ppc_resample.R"))
 source(here("scripts/tidymodels/3_create_recipes.R"))
@@ -13,5 +19,3 @@ source(here("scripts/tidymodels/7_create_training_scripts.R"))
 source(here("output/tidymodels/training_scripts/rf_spi_5.R"))
 source(here("output/tidymodels/training_scripts/rf_spi_27.R"))
 source(here("output/tidymodels/training_scripts/rf_spi_135.R"))
-
-# model fits saved to 
